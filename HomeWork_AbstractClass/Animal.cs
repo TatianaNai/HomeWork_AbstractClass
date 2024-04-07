1a95b14ca6d82e3167c98a35a5473d8b1484a734
Tatiana <134440396+TatianaNai@users.noreply.github.com>
2024-04-07 14:33:30 +0300

    Add files via upload

---
+++ HomeWork_AbstractClass/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_AbstractClass
+{
+    public abstract class Animal
+    {
+        protected string Name { get; set; }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public abstract void Eat();
+    }
+}
---
+++ HomeWork_AbstractClass/Program.cs
@@ -0,0 +1,11 @@
+﻿using HomeWork_AbstractClass;
+
+Console.WriteLine("Write the name of the dog");
+
+Dog dog = new Dog();
+dog.SetName(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"The name of the dog is {dog.GetName()}.");
+dog.Eat();
+
